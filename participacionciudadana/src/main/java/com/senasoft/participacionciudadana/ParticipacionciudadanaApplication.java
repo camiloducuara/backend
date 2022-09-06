ce218d1bd697d182570373d11ad2d00bad9047fb
= <=>
2022-09-06 18:17:46 -0500

    Default files

---
+++ participacionciudadana/src/main/java/com/senasoft/participacionciudadana/ParticipacionciudadanaApplication.java
@@ -0,0 +1,13 @@
+package com.senasoft.participacionciudadana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParticipacionciudadanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParticipacionciudadanaApplication.class, args);
+	}
+
+}
